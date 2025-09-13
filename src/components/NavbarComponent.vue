a24d33ecf7e0ff47c5aaf7fd6d678962264ee37a
Fabricio Barrientos Takata <fbarrientostakata@gmail.com>
2025-09-13 01:52:52 -0400

    redcors

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,30 +1,41 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from "vue"
 import logo from "../assets/logo.svg";
+
 const activeSection = ref("inicio")
 const isMenuOpen = ref(false)
-
+const pathname = ref("");
 const isMegaOpen = ref(false)
 
 function toggleMegaMenu() {
+  activeSection.value="servicios"
   isMegaOpen.value = !isMegaOpen.value
+}
+
+function activeVentas(){
+  activeSection.value="servicios"
+  window.location.href = "/urbanizaciones/lapaz/"
+  document.getElementById("servicios")
 }
 
 
 function scrollToSection(id) {
+  
     const el = document.getElementById(id)
+    
     if (el) {
+      
         el.scrollIntoView({ behavior: "smooth" })
     }
-    isMenuOpen.value = false // cierra menú móvil al hacer click
+    isMenuOpen.value = false 
 }
 
 let observer
 
 onMounted(() => {
     const sections = document.querySelectorAll("section[id]")
-
-    observer = new IntersectionObserver(
+    if(activeSection.value==="inicio"){
+       observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
@@ -40,6 +51,10 @@ onMounted(() => {
 
 
     sections.forEach((sec) => observer.observe(sec))
+    }else{
+      activeSection.value="servicios"
+    }
+   
 })
 
 onBeforeUnmount(() => {
@@ -85,24 +100,30 @@ onBeforeUnmount(() => {
                 <ul
                     class="flex flex-col justify-center items-start md:items-center p-4 md:p-0 mt-1 gap-3 font-medium md:space-x-12 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                     <li>
-                        <button @click="scrollToSection('inicio')" :class="[
+                        <button  @click="scrollToSection('inicio')" :class="[
                             'transition-all duration-300 cursor-pointer font-semibold text-sm tracking-wider uppercase',
-                            activeSection === 'inicio' ? 'text-amber-300 font-bold' : 'text-slate-100 hover:text-amber-300'
+                            activeSection==='inicio' && isMegaOpen===false ? 'text-amber-300 font-bold' : 'text-slate-100 hover:text-amber-300'
                         ]">
                             INICIO
                         </button>
                     </li>
                     <li>
-                    <button @click="toggleMegaMenu" 
+                 <button 
+  @click="() => { toggleMegaMenu();}" 
   :class="[
-    'flex gap-1 justify-center items-center  transition-all duration-300 cursor-pointer font-semibold text-sm tracking-wider uppercase',
-    activeSection === 'servicios' ? 'text-amber-300 font-bold' : 'text-slate-100 hover:text-amber-300'
-  ]">
+    'flex gap-1 justify-center items-center transition-all duration-300 cursor-pointer font-semibold text-sm tracking-wider uppercase',
+    activeSection === 'servicios' 
+      ? 'text-amber-300 font-bold' 
+      : 'text-slate-100 hover:text-amber-300'
+  ]"
+>
   SERVICIOS
   <svg class="w-2.5 h-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
   </svg>
 </button>
+
+
 
                 </li>
                     
@@ -131,14 +152,14 @@ onBeforeUnmount(() => {
   <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
     <ul>
       <li>
-        <a href="/urbanizaciones/lapaz/" class="block p-3 rounded-lg hover:bg-slate-950 dark:hover:bg-gray-700">
+        <button @click="activeVentas" class="block p-3 rounded-lg hover:bg-slate-950 dark:hover:bg-gray-700">
           <div class=" text-amber-400 font-bold flex gap-2 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="currentColor" fill-rule="evenodd" d="M12.398 17.804C13.881 17.034 19 14.016 19 9A7 7 0 1 0 5 9c0 5.016 5.119 8.035 6.602 8.804a.86.86 0 0 0 .796 0M12 12a3 3 0 1 0 0-6a3 3 0 0 0 0 6" clip-rule="evenodd"/><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.062 16.5c.615.456.938.973.938 1.5s-.323 1.044-.938 1.5c-.614.456-1.498.835-2.562 1.098S13.229 21 12 21s-2.436-.139-3.5-.402s-1.948-.642-2.562-1.098C5.323 19.044 5 18.527 5 18s.323-1.044.938-1.5"/></g></svg>
-            VENTA DE LOTES</div>
+            VENTA DE LOTES Y VIVIENDAS</div>
           <span class="text-xs text-white ">
-            Terrenos residenciales en las mejores ubicaciones de Bolivia con servicios básicos garantizados.
+            Terrenos y viviendas residenciales en las mejores ubicaciones de Bolivia con servicios básicos garantizados.
           </span>
-        </a>
+        </button>
       </li>
       <li>
         <a href="/consultoria/" class="block p-3 rounded-lg hover:bg-slate-950 dark:hover:bg-gray-700">
